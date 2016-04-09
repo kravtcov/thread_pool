23add7b5c376e845b283684a0d0b1780e201a9dc
Alexander Kravtsov <alexander.kravtcov@gmail.com>
2016-04-09 20:57:31 +0300

    Revert "Add dump() and overloading of out operator"
    This reverts commit 9e0b66bd886a510e8165f9933b4a7faa67190cc6.

--- hometask_1/main.cpp
+++ hometask_1/main.cpp
@@ -5,17 +5,14 @@
 
 int main() {
     MySet<int> first;
-    std::cout << "first after () " << first << std::endl << std::endl;
+    std::cout << "first " << first << std::endl;
 
     first.insert(33);
-    std::cout << "first after 33 " << first << std::endl << std::endl;
+    std::cout << "first " << first << std::endl;
 
     first.insert(44);
-    std::cout << "first after 44 " << first << std::endl << std::endl;
     first.insert(33);
-    std::cout << "first after 33 " << first << std::endl << std::endl;
-    first.insert(55);
-    std::cout << "first after 55 " << first << std::endl << std::endl;
+    std::cout << "first " << first << std::endl;
 
     return 0;
 }
