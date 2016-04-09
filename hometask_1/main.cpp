39762c5eb9f604d9c836a615029a1b58e699e6d0
Alexander Kravtsov <alexander.kravtcov@gmail.com>
2016-04-09 23:53:20 +0300

    Add reallocation

--- hometask_1/main.cpp
+++ hometask_1/main.cpp
@@ -5,14 +5,17 @@
 
 int main() {
     MySet<int> first;
-    std::cout << "first " << first << std::endl;
+    std::cout << "first after () " << first << std::endl << std::endl;
 
     first.insert(33);
-    std::cout << "first " << first << std::endl;
+    std::cout << "first after 33 " << first << std::endl << std::endl;
 
     first.insert(44);
+    std::cout << "first after 44 " << first << std::endl << std::endl;
     first.insert(33);
-    std::cout << "first " << first << std::endl;
+    std::cout << "first after 33 " << first << std::endl << std::endl;
+    first.insert(55);
+    std::cout << "first after 55 " << first << std::endl << std::endl;
 
     return 0;
 }
